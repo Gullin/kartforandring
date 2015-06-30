c88943483948236b32c414dd282d61b14e6196d1
Gullin <krich_80@hotmail.com>
2015-06-30 10:23:14 +0200

    Deploy v0.1

--- kartforandring/Properties/AssemblyInfo.cs
+++ kartforandring/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("kartforandring")]
+[assembly: AssemblyTitle("Kartförändringar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("kartforandring")]
+[assembly: AssemblyProduct("Kartförändringar")]
 [assembly: AssemblyCopyright("Copyright © Christian Gullin")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
